e2d62bb4e44ca886be7e8fe2ac2916c7d60db050
Byron Ruth <b@devel.io>
2016-02-24 07:10:40 -0500

    Exclude incompatible test matrices
    Update test script to account for all versions greater than 1.7
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- test_suite.py
+++ test_suite.py
@@ -2,9 +2,12 @@ import os
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
-import django
-if django.get_version().startswith('1.7'):
+import django  # noqa
+
+if django.__version__ >= (1, 7):
     django.setup()
 
-from django.core import management
+
+from django.core import management  # noqa
+
 management.call_command('test', 'tests')
