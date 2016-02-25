1783fe847db8d826623e60f1bc370d62642fb47b
Byron Ruth <b@devel.io>
2016-02-25 06:44:20 -0500

    1.2.0 Release
    Signed-off-by: Byron Ruth <b@devel.io>

--- preserialize/__init__.py
+++ preserialize/__init__.py
@@ -1,8 +1,8 @@
 __version_info__ = {
     'major': 1,
-    'minor': 1,
-    'micro': 1,
-    'releaselevel': 'beta',
+    'minor': 2,
+    'micro': 0,
+    'releaselevel': 'final',
     'serial': 1
 }
 
